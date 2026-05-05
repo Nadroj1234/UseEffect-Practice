9012bc2dfd2167e3b667993501422a54d4c12673
Jordan Klemm <jordank06759@gmail.com>
2026-05-05 14:26:39 -0400

    updated team names and logos

--- ue_practice/src/datePicker.jsx
+++ ue_practice/src/datePicker.jsx
@@ -1,17 +1,31 @@
 import { useEffect, useState } from "react";
-
+import "./index.css";
 function SearchUSers() {
   const link =
-    "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard?dates=";
+    "http://site.api.espn.com/apis/site/v2/sports/soccer/usa.1/scoreboard?dates=";
   const [date, setDate] = useState("");
   const [results, setResults] = useState([]);
 
   useEffect(() => {
     if (date === "") return;
+
     fetch(link + date.split("-").join(""))
       .then((res) => res.json())
-      .then((results) => {
-        setResults(results);
+      .then((data) => {
+        const teams = [];
+        const events_list = [];
+
+        for (let e of data.events) {
+          events_list.push(e.name[0]);
+          for (let c of e.competitions) {
+            for (let a of c.competitors) {
+              teams.push(a.team);
+            }
+          }
+        }
+
+        setResults(teams, events_list);
+        console.log(teams);
       })
       .catch((err) => console.error(err));
   }, [date]);
@@ -25,7 +39,15 @@ function SearchUSers() {
           placeholder="Choose A Date"
           onChange={(e) => setDate(e.target.value)}
         />
-        <div>{JSON.stringify(results, 2)}</div>
+        {/* <div>{JSON.stringify(results, 2)}</div> */}
+        <div className="results_div">
+          {results.map((team, index) => (
+            <div key={index}>
+              <p>{team.name}</p>
+              <img src={team.logo} alt="" />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
